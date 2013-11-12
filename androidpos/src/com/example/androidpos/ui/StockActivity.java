7988de963f9dd5a43263132c2355df3ad66a7751
Eknarin <oat-et@hotmail.com>
2013-11-12 17:17:21 +0700

    Init StockActivity

--- androidpos/src/com/example/androidpos/ui/StockActivity.java
+++ androidpos/src/com/example/androidpos/ui/StockActivity.java
@@ -1,5 +1,32 @@
 package com.example.androidpos.ui;
 
-public class StockActivity {
+import java.util.HashMap;
+import java.util.List;
 
+import com.example.androidpos.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class StockActivity extends Activity{
+	private List<HashMap<String,String>> listmap;
+	private ListView listview;
+	private SimpleAdapter simAdapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stock);
+		listview = (ListView) findViewById(R.id.itemlist);
+	}
+	private void updateListView() {
+
+		simAdapter = new SimpleAdapter(this, listmap,
+				R.layout.activity_column_stock, new String[] { "id", "name",
+				"cost","quantity"},
+				new int[] {R.id.colId, R.id.colName, R.id.colCost, R.id.colQuantity});
+		listview.setAdapter(simAdapter);
+	}
 }
