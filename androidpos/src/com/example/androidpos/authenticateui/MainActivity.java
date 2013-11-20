1907fa00da0c78f2ad1592b17fc7d12cceefb9ee
earthng'eiei <saritsuriya@hotmail.com>
2013-11-20 14:44:18 +0700

    Add all tab to replace button

--- androidpos/src/com/example/androidpos/authenticateui/MainActivity.java
+++ androidpos/src/com/example/androidpos/authenticateui/MainActivity.java
@@ -12,12 +12,16 @@ import com.example.androidpos.saleui.SaleActivity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
 	private Button inventoryBtn;
 	private Button saleBtn;
@@ -34,42 +38,66 @@ public class MainActivity extends Activity {
 		SaleLedger.initInstance(dbh);
 		
 		
-		initComponent();
+//		initComponent();
 		
-		inventoryBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent inventory = new Intent(MainActivity.this,InventoryActivity.class);
-				startActivity(inventory);				
-			}
-		});
-		
-		saleBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent sale = new Intent(MainActivity.this,SaleActivity.class);
-				startActivity(sale);				
-			}
-		});
-		
-		reportBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent report = new Intent(MainActivity.this,SaleLedgerActivity.class);
-				startActivity(report);				
-			}
-		});
+//		inventoryBtn.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent inventory = new Intent(MainActivity.this,InventoryActivity.class);
+//				startActivity(inventory);				
+//			}
+//		});
+//		
+//		saleBtn.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent sale = new Intent(MainActivity.this,SaleActivity.class);
+//				startActivity(sale);				
+//			}
+//		});
+//		
+//		reportBtn.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent report = new Intent(MainActivity.this,SaleLedgerActivity.class);
+//				startActivity(report);				
+//			}
+//		});
 
+		TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		
+		TabSpec tab1 = tabHost.newTabSpec("First");
+		TabSpec tab2 = tabHost.newTabSpec("Second");
+		TabSpec tab3 = tabHost.newTabSpec("Third");
+		
+		tab1.setIndicator("Inventory");
+		Intent inventoryTabAc = new Intent(this,InventoryActivity.class);
+		tab1.setContent( inventoryTabAc.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+		tab1.setContent( inventoryTabAc );
+		Log.d("tabhost", tab1.toString());
+		tab2.setIndicator("Sale");
+		Intent saleAc = new Intent(this,SaleActivity.class);
+		tab2.setContent( saleAc.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+		tab2.setContent( saleAc );
+		
+		tab3.setIndicator("Report");
+		Intent reportAc = new Intent(this,SaleLedgerActivity.class);
+		tab3.setContent( reportAc.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+		tab3.setContent( reportAc );
+		
+		tabHost.addTab(tab1);
+		tabHost.addTab(tab2);
+		tabHost.addTab(tab3);
 	}
 
-	private void initComponent() {
-		inventoryBtn = (Button)findViewById(R.id.inventory_button);
-		saleBtn = (Button)findViewById(R.id.sale_button);
-		reportBtn = (Button)findViewById(R.id.report_button);
-	}
+//	private void initComponent() {
+//		inventoryBtn = (Button)findViewById(R.id.inventory_button);
+//		saleBtn = (Button)findViewById(R.id.sale_button);
+//		reportBtn = (Button)findViewById(R.id.report_button);
+//	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		return true;
