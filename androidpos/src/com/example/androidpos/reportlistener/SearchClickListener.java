e08bf787ee32999a08094c9603325ec9ce7e1792
Natcha119 <natcha119@hotmail.com>
2013-11-23 15:45:30 +0700

    remove some conflict files

---
+++ androidpos/src/com/example/androidpos/reportlistener/SearchClickListener.java
@@ -0,0 +1,51 @@
+package com.example.androidpos.reportlistener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.example.androidpos.date.DateStrategy;
+import com.example.androidpos.reportui.SaleLedgerActivity;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Spinner;
+
+public class SearchClickListener implements OnClickListener {
+
+	private SaleLedgerActivity sla;
+	private Spinner date;
+	private Spinner month;
+	private Spinner year;
+	private List<HashMap<String, String>> listmap;
+	
+	public SearchClickListener(SaleLedgerActivity sla,
+			Spinner date, Spinner month, Spinner year,
+			List<HashMap<String, String>> listmap) {
+		this.sla = sla;
+		this.date = date;
+		this.month = month;
+		this.year = year;
+		this.listmap = listmap;
+	}
+
+	@Override
+	public void onClick(View v) {
+		String d = date.getSelectedItem().toString();
+		String m = date.getSelectedItem().toString();
+		String y = date.getSelectedItem().toString();
+		
+		String checkDate = d + "." + m + "." + y;
+		
+		List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+		
+		for ( HashMap<String, String> item : listmap ) {
+			if ( DateStrategy.isDate(checkDate, item.get("lastedit")) )
+					list.add(item);
+		}
+		
+		sla.updateListView(list);
+	}
+
+	
+}
